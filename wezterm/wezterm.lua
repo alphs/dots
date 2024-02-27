5a26c90f6a1d5f121f4406ec3a5c18c2ec132624
aaa <49196143+alphs@users.noreply.github.com>
2024-02-27 20:00:26 +0100

    Move nonicons folder to ./installs

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,33 +1,34 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = {}
 
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 --{{ Misc
 config.send_composed_key_when_left_alt_is_pressed = true
-config.send_composed_key_when_right_alt_is_pressed = false 
+config.send_composed_key_when_right_alt_is_pressed = false
 --}} Misc
 
 --{{ Looks
 
 -- 'Catppuccin [ Frappe, Latte, Macchiato, Mocha ]'
 -- 'Catppuccin [ Frappé, Latte, Macchiato, Mocha ] (Gough)'
-config.color_scheme = 'Catppuccin Macchiato'-- (Gogh)'
+config.color_scheme = "Catppuccin Macchiato" -- (Gogh)'
 
-config.font_locator = 'ConfigDirsOnly'
-config.font_dirs = { '~/git/nonicons' }
-config.font = wezterm.font_with_fallback {
-  'JetBrains Mono', -- included by default
-  'nonicons',
-}
+config.font_locator = "ConfigDirsOnly"
+config.font_dirs = { "../installs/fonts_icons/JetBrainsMonoNerdFont", "../installs/fonts_icons/nonicons/dist" }
+config.font = wezterm.font_with_fallback({
+	"JetBrainsMono Nerd Font",
+	-- "JetBrains Mono", -- included by default
+	"nonicons",
+})
 
 -- default is true, has more "native" look; very round and strange
-config.use_fancy_tab_bar =  false
+config.use_fancy_tab_bar = false
 
 config.enable_scroll_bar = false
 config.tab_bar_at_bottom = false
@@ -56,4 +57,3 @@ config.window_background_image_hsb = {
 --}} Looks
 
 return config
-
