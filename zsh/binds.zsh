b9c3014df3113d87bda0266eafa5986a8d71f686
aaa <49196143+alphs@users.noreply.github.com>
2025-07-21 19:41:03 +0000

    zsh: ^P ^N ignore line(s) only history, <shift+tab> reverse menu complete

--- zsh/binds.zsh
+++ zsh/binds.zsh
@@ -3,14 +3,13 @@
 autoload -Uz edit-command-line
 zle -N edit-command-line
 bindkey -M main '^X^X' edit-command-line
-bindkey -M main '^X^E' exchange-point-and-mark
 
 export KEYTIMEOUT=20
 set -o vi
 
 # 'main' is viins if 'set -o vi', man zshzle
-bindkey -M main '^P' up-line-or-history  # history-beginning-search-backward
-bindkey -M main '^N' down-line-or-history  # history-beginning-search-forward
+bindkey -M main '^P' up-history
+bindkey -M main '^N' down-history
 bindkey -M main '^A' beginning-of-line
 bindkey -M main '^E' end-of-line
 bindkey -M main '^B' backward-char
@@ -19,6 +18,7 @@ bindkey -M main '^D' delete-char
 bindkey -M main '^K' kill-line
 bindkey -M main '^U' backward-kill-line
 bindkey -M main '^W' backward-kill-word
-bindkey -M main '^Y' yank
-bindkey -M main '^R' history-incremental-search-backward
 
+bindkey -M main '^[[Z' reverse-menu-complete    # 'shift-tab'
+
+bindkey -M main '^R' history-incremental-search-backward # this will be overwritten by fzf if present
